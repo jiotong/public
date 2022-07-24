0fb233465216f902bda8a42a49232185c2070d9c
jiotong <72918802+jiotong@users.noreply.github.com>
2022-07-24 13:50:25 +0800

    Update one.sh

--- one.sh
+++ one.sh
@@ -7,14 +7,12 @@ ip=$(ifconfig|grep 'inet '|grep -v '127.0'|xargs|awk -F '[ :]' '{print $3}')
 
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}错误: ${plain} 必须使用root用户运行此脚本！\n" && exit 1
-cd /root/ &&
-curl -sL https://github.com/eysp/public/archive/public.tar.gz | tar xz
+cd /opt/ &&
+curl -sL https://github.com/jiotong/public/archive/public.tar.gz | tar xz
 rm -rf public
 mv public-public public
 docker stop portainer
 docker rm portainer
-docker rmi portainer/portainer:linux-arm64
-docker rmi portainer/portainer-ce:linux-arm64
-docker run -d --restart=always --name="portainer" -p 9999:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -v /root/public:/public portainer/portainer-ce:linux-arm64
-echo -e "${red} portainer部署成功，访问 "${ip}":9999"
+docker run -d --restart=always --name="portainer" -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -v /opt/public:/public portainer/portainer-ce:2.9.1
+echo -e "${red} portainer部署成功，访问 "${ip}":9000"
 echo -e "${plain}"
